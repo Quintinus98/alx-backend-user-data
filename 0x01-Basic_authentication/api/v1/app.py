bca60df3829193d2b229bd8520424ae359114dfc
David <obododavidsop@gmail.com>
2024-06-04 15:48:48 +0100

    all updated

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,7 +19,7 @@ if auth == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
 
     auth = BasicAuth()
-elif auth == "auth":
+else:
     from api.v1.auth.auth import Auth
 
     auth = Auth()
