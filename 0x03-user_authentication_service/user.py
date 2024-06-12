50c6f2f9fc9e279ca69b7c8d8c0d2847d606fb33
David <obododavidsop@gmail.com>
2024-06-12 19:05:00 +0100

    user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """ SQLAlchemy class """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, Column
 from typing import Optional
 
 
@@ -14,8 +14,13 @@ class User(Base):
 
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String(250))
-    hashed_password: Mapped[str] = mapped_column(String(250))
-    session_id: Mapped[Optional[str]] = mapped_column(String(250))
-    reset_token: Mapped[Optional[str]] = mapped_column(String(250))
+    # id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    # email: Mapped[str] = mapped_column(String(250))
+    # hashed_password: Mapped[str] = mapped_column(String(250))
+    # session_id: Mapped[Optional[str]] = mapped_column(String(250))
+    # reset_token: Mapped[Optional[str]] = mapped_column(String(250))
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
