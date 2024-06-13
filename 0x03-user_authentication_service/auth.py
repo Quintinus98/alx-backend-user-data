cdad3326f5b55560d809468d474d1b944ea6f65c
David <obododavidsop@gmail.com>
2024-06-13 17:54:47 +0100

    register user update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """Authentication"""
 import bcrypt
 from db import DB
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 from user import User
 
 
@@ -27,11 +27,11 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """Returns a User object"""
         try:
-            self._db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
         except NoResultFound:
             hashed_password = _hash_password(password)
-            new_user = self._db.add_user(email, hashed_password)
+            user = self._db.add_user(email, hashed_password)
 
-            return new_user
+            return user
         else:
             raise ValueError(f"User {email} already exists")
