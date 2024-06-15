0137287f0ab157d604dd12b579501a189fb744fc
David <obododavidsop@gmail.com>
2024-06-15 05:17:59 +0100

    13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,3 +72,7 @@ class Auth:
         except NoResultFound:
             return None
         return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys a session"""
+        self._db.update_user(user_id, session_id=None)
