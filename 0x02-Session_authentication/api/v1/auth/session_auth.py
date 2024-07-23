284dd55a471759f885af12ac52af880044f7fa34
David <obododavidsop@gmail.com>
2024-07-23 09:39:45 +0100

    session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,9 +6,18 @@ from flask import request
 from api.v1.auth.auth import Auth
 import base64
 from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session Authentication"""
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for a user id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
