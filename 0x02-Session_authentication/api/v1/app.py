ea5b81cbc2becdf1f1ed51ef8ecffe88fad1052b
David <obododavidsop@gmail.com>
2024-07-23 16:56:40 +0100

    before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,6 +56,7 @@ def filter_request():
         "/api/v1/status/",
         "/api/v1/unauthorized/",
         "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/",
     ]
     if not auth.require_auth(request.path, excluded_paths):
         return
@@ -63,6 +64,11 @@ def filter_request():
         abort(401)
     if not auth.current_user(request):
         abort(403)
+    if (
+        auth.authorization_header(request) is None
+        and auth.session_cookie(request) is None
+    ):
+        abort(401)
     request.current_user = auth.current_user(request)
 
 
